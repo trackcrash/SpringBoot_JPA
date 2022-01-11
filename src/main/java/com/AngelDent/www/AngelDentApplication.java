d35c538a1d14f1364d8d1379de4336959c3e6114
trackcrash <auroknh@gmail.com>
2022-01-11 17:23:37 +0900

    Initial commit

---
+++ src/main/java/com/AngelDent/www/AngelDentApplication.java
@@ -0,0 +1,13 @@
+package com.AngelDent.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngelDentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngelDentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AngelDent/www/AngelDentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AngelDent.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngelDentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
